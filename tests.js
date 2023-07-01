c2767209d3543015157e4f4161926548be219f1c
Rod Vagg <rod@vagg.org>
2023-07-01 21:47:00 +1000

    feat!: esm, async/await, updates and add linting

--- tests.js
+++ tests.js
@@ -1,42 +1,29 @@
-const assert = require('assert')
-const mkfiletree = require('mkfiletree')
-const readfiletree = require('./readfiletree')
+import assert from 'assert'
+import * as mkfiletree from 'mkfiletree'
+import { readfiletree } from './readfiletree.js'
+
 function mkfixture () { // make a new copy each time, ensure no edits at any depth
   return {
     'foo.txt': 'FOO!',
-    'bar': {
-      'bang': {
+    bar: {
+      bang: {
         '1.dat': '1\n',
         '2.dat': '2\n\n',
         '3.dat': '3\n\n\n'
       },
-      'BAM': 'WOO HOO!!!\n'
+      BAM: 'WOO HOO!!!\n'
     }
   }
 }
 
-async function test (asPromises) {
+async function test () {
   const dir = await mkfiletree.makeTemp('readfiletree_test', mkfixture())
   assert(dir)
-
-  if (!asPromises) {
-    readfiletree(dir, (err, obj) => {
-      assert(!err)
-      verify(obj)
-    })
-  } else {
-    readfiletree(dir)
-      .then(verify)
-  }
-
-  function verify (obj) {
-    assert.deepStrictEqual(obj, mkfixture())
-  }
+  const obj = await readfiletree(dir)
+  assert.deepStrictEqual(obj, mkfixture())
 }
 
-test(false).then(() => {
-  return test(true)
-}).catch((err) => {
+test().catch((err) => {
   console.error(err)
   process.exit(1)
 })
